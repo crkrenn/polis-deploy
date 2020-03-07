513a3536ebe018df4dba4fe123d20146faae20e4
Chris Krenn <crkrenn@gmail.com>
2020-03-07 09:08:09 -0800

    updated polis to run with docker-compose

---
+++ polis_docker_local/TODO.md
@@ -0,0 +1,1 @@
+use templates to keep settings in polis.config.js and env_dev consistent {{{}}}
--- polis_docker_local/polis.config_docker.js
+++ polis_docker_local/polis.config_docker.js
@@ -2,19 +2,10 @@
 module.exports = {
 
   domainWhitelist: [
-    "^ec2-34-215-51-34.us-west-2.compute.amazonaws.com$",
-    "^preprod.dgpsp.s3.s3-website-us-west-2.amazonaws.com$",
-    "^http://dgpsp-server2-staging.herokuapp.com$",
     "^localhost$",
     "^server$",
     "^client$",
     "^admin$",
-    "^172.25.0",        
-    "^172.28.1",        
-    "^52.32.106.92$",
-    "^polis2.democracygps.org$",
-    "^54.214.183.214$",
-    "^polis.democracygps.org$",
     "^192\\.168\\.1\\.140$",
     "^pol\\.is",
     ".+\\.pol\\.is$",
@@ -23,11 +14,18 @@ module.exports = {
   ],
 
   // Point to a polisServer instance (local recommended for dev)
-  SERVICE_URL: "http://localhost:5000", // local server; recommended for dev
+//  SERVICE_URL: "http://localhost:5000", // local server; recommended for dev
+  SERVICE_URL: "http://server:5000", // local server; recommended for dev
   //SERVICE_URL: "https://preprod.pol.is",
 
   // Note that this must match the participation client port specified in polisServer instance
   PORT: 5001,
+  CLIENT_PORT: 5001,
+  ADMIN_PORT: 5002,
+  // CLIENT_HOST: '0.0.0.0',
+  // ADMIN_HOST: '0.0.0.0',
+  CLIENT_HOST: 'client',
+  ADMIN_HOST: 'admin',
 
   DISABLE_INTERCOM: true,
   DISABLE_PLANS: true,
